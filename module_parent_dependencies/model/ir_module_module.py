613420288d6df8399b3df1eace3fa5d8b9d3c0e2
Sylvain LE GAL <sylvain.legal@grap.coop>
2015-07-17 18:39:25 +0200

    [WIP]

--- module_parent_dependencies/model/ir_module_module.py
+++ module_parent_dependencies/model/ir_module_module.py
@@ -20,54 +20,52 @@
 #
 ##############################################################################
 
-from openerp.osv.orm import Model
-from openerp.osv import fields
+from openerp import api, fields
+from openerp.models import Model
 
 
-class module(Model):
+class IrModuleModule(Model):
     _inherit = 'ir.module.module'
 
     # Field function Section
-    def _get_all_installed_parent_ids(
-            self, cr, uid, ids, field_name, arg, context=None):
-        res = self._get_direct_installed_parent_ids(
-            cr, uid, ids, field_name, arg, context=context)
-        for id in ids:
-            parent_ids = list(res[id])
-            undirect_parent_ids = self._get_all_installed_parent_ids(
-                cr, uid, res[id], field_name, arg, context=context)
-            for parent_id in parent_ids:
-                res[id] += undirect_parent_ids[parent_id]
-            res[id] = list(set(res[id]))
-        return res
+#    def _get_all_installed_parent_ids(
+#            self, cr, uid, ids, field_name, arg, context=None):
+#        res = self._get_direct_installed_parent_ids(
+#            cr, uid, ids, field_name, arg, context=context)
+#        for id in ids:
+#            parent_ids = list(res[id])
+#            undirect_parent_ids = self._get_all_installed_parent_ids(
+#                cr, uid, res[id], field_name, arg, context=context)
+#            for parent_id in parent_ids:
+#                res[id] += undirect_parent_ids[parent_id]
+#            res[id] = list(set(res[id]))
+#        return res
 
-    def _get_direct_installed_parent_ids(
-            self, cr, uid, ids, field_name, arg, context=None):
-        res = {}
-        imd_obj = self.pool['ir.module.module.dependency']
-        for imm in self.browse(cr, uid, ids, context=context):
-            imd_ids = imd_obj.search(
-                cr, uid, [('name', '=', imm.name)], context=context)
-            tmp = imd_obj.read(
-                cr, uid, imd_ids, ['module_id'], context=context)
-            imm_ids = [x['module_id'][0] for x in tmp]
-            # Select only non uninstalled module
-            imm_ids = self.search(
-                cr, uid, [
+    @api.multi
+    def _get_all_installed_parent_ids(self):
+        print self
+        for rec in self:
+            parent_ids = rec.direct_installed_parent_ids
+            import pdb;pdb.set_trace()
+
+    @api.multi
+    def _get_direct_installed_parent_ids(self):
+        immd_obj = self.env['ir.module.module.dependency']
+        for rec in self:
+            immd_ids = immd_obj.search([('name', '=', rec.name)])
+            imm_ids = [x.module_id.id for x in immd_ids]
+            imm_lst = self.search([
                     ('id', 'in', imm_ids),
-                    ('state', 'not in', ['uninstalled', 'uninstallable'])],
-                context=context)
-            res[imm.id] = imm_ids
-        return res
+                    ('state', 'not in', ['uninstalled', 'uninstallable'])])
+            rec.all_installed_parent_ids = [x.id for x in imm_lst]
 
     # Column Section
-    _columns = {
-        'direct_installed_parent_ids': fields.function(
-            _get_direct_installed_parent_ids, type='many2many',
-            relation='ir.module.module',
-            string='Direct Parent Installed Modules'),
-        'all_installed_parent_ids': fields.function(
-            _get_all_installed_parent_ids, type='many2many',
-            relation='ir.module.module',
-            string='Direct and Indirect Parent Installed Modules'),
-    }
+    direct_installed_parent_ids = fields.Many2many(
+        compute=_get_direct_installed_parent_ids,
+        comodel_name='ir.module.module',
+        string='Direct Parent Installed Modules')
+
+    all_installed_parent_ids = fields.Many2many(
+        compute=_get_all_installed_parent_ids,
+        comodel_name='ir.module.module',
+        string='Direct and Indirect Parent Installed Modules')
